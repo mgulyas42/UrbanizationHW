701383ad0e9cda3319f37263824898234ff26355
buboribencebarnabas <buboribencebarnabas@lechnerkozpont.hu>
2020-11-11 16:38:00 +0100

    TODO hozzáadása backendhez

--- restBE/src/app.controller.ts
+++ restBE/src/app.controller.ts
@@ -12,33 +12,42 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  @Get("/debug/:z/:x/:y")
+  debugTiles(@Req() request, @Res() res) {
+    Logger.debug(request.params);
+    Logger.debug(`longitude: ${this.tile2long(request.params.x, request.params.z)}`);
+    Logger.debug(`longitude: ${this.tile2lat(request.params.y, request.params.z)}`);
+
+    res.send('almafa');
+  }
+
+
   @Get("/proxy/:z/:x/:y")
   getImage(@Req() request, @Res() res) {//10339,7330
-    //if(request.params.z == 14 && request.params.x == 10339 && request.params.y == 7330) {
-      console.log('SIKEEEEER');
-      /*Jimp.read(resolve(join(__dirname, `./datas/1/01.bmp`))).then(image => {
-        console.log(image);
-        image.res
-          res.download(image);
-      });*/
 
+    /*TODO:
+      Vectorlayer, arra helyezek egy pontot, mint a markert, oda rakjuk be a layert.
+      Az eredeti képek a google térképről származnak, nem kell pontosan megjelenniük.
+      A skálázással / zoomszinttel frontend oldalon kell majd játszani, a kép középpontja az adott latlong
+     */
 
-      Jimp.read(resolve(join(__dirname, `./datas/1/01.bmp`)), function (err, test:any) {
-          if (err) throw err;
-          /*test.resize(256, 256)
-              .quality(50)
-              .write(__dirname + "./new.jpg");*/
-          res.download(resolve(join(__dirname + "./new.jpg")));
-      });
-      //res.download(resolve(join(__dirname, `./datas/1/01.bmp`)));
-      return;
-    //}
+    if(request.params.z == 16 && request.params.x == 36151 && request.params.y == 22999) {
+      res.download(resolve(join(__dirname, `./datas/1/01.bmp`)));
+      Logger.debug(request.params);
+      Logger.debug(`calculated longitude: ${this.tile2long(request.params.x, request.params.z)}`);
+      Logger.debug(`calculated latitude: ${this.tile2lat(request.params.y, request.params.z)}`);
+      Logger.debug(`Original latLong: ${[47.190287,18.584784]}`);
+    }
+    else {
+      res.send('almafa');
+    }
+  }
 
-
-    Logger.debug('send 01.bmp');
-    Logger.debug(request.params)
-    //res.download(resolve(join(__dirname, `./datas/${request.params.x}/01.bmp`)));
-    //res.type('image/bmp').send(createReadStream(resolve(join(__dirname, './datas/1/01.bmp'))));
-    res.send('almafa');
+  tile2long(x,z) {
+    return (x/Math.pow(2,z)*360-180);
+  }
+  tile2lat(y,z) {
+    const n = Math.PI-2*Math.PI*y/Math.pow(2,z);
+    return (180/Math.PI*Math.atan(0.5*(Math.exp(n)-Math.exp(-n))));
   }
 }
